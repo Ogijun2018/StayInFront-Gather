e8075c900b43229b8b30322f7f2074574ad4f813
Jun Ogino <ogijun.mk@hotmail.co.jp>
2023-11-08 19:33:58 +0900

    refactor: organize WindowController

--- StayInFront/WindowController.swift
+++ StayInFront/WindowController.swift
@@ -12,23 +12,26 @@ class WindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-        window?.level = .floating
+        // MARK: setting closure
+        let appDelegate = NSApp.delegate as? AppDelegate
+        appDelegate?.toggleMenuHandler = { [weak self] in
+            self?.changeWindowLebel()
+        }
+        // MARK: setting webView
         let webView = WKWebView(frame: self.window!.contentView!.bounds)
         webView.autoresizingMask = [.width, .height]
         // gather townのWeb版は現在Chromeまたはfirefoxのみで動作する（Safariは推奨環境ではないが動作する）
         // それ以外のブラウザでアクセスすると使用できないため、UserAgentを書き換えてSafariとして認識させる
         webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15"
-        window!.contentView!.addSubview(webView)
-
-        let appDelegate = NSApp.delegate as? AppDelegate
-        appDelegate?.toggleMenuHandler = { [weak self] in
-            self?.changeWindowLebel()
-        }
-
         if let url = URL(string: "https://app.gather.town/app") {
             let request = URLRequest(url: url)
             webView.load(request)
         }
+        // MARK: setting window
+        guard let window else { return }
+        window.title = "Gather(StayInWindow)"
+        window.contentView?.addSubview(webView)
+        window.level = .floating
     }
 
     private func changeWindowLebel() {
