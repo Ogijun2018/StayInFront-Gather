7602f8cdbd06f819034289a22b2ac61aad0b0f95
Jun Ogino <ogijun.mk@hotmail.co.jp>
2023-11-08 11:00:15 +0900

    Initial Commit

---
+++ StayInFront/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StayInFront
+//
+//  Created by jun.ogino on 2023/11/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
